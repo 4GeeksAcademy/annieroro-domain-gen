9567b2f2684edbc69955da0b9ca877195b0787ea
Annie Roldán Rodríguez <149087336+AnnieRoro@users.noreply.github.com>
2023-12-10 16:52:32 +0000

    annieroro-domain-gen

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let ext = ['.es', '.com'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i = 0; i < pronoun.length; i++){
+    for (let i2 = 0; i2 < adj.length; i2++){
+      for(let i3 = 0; i3 < noun.length; i3++){
+        for(let i4 = 0; i4 < ext.length; i4++){
+          console.log(pronoun[i]+adj[i2]+noun[i3]+ext[i4]);
+
+        }
+      }
+    }
+  }
+
+
